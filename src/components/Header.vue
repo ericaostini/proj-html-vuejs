118a1a3c646d80ecaed8f066570059889c3e65dc
ericaostini <ericaostini14@gmail.com>
2023-11-21 12:53:07 +0100

    add background image and color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,22 @@
 <template>
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-4 col-md-5 col-sm-5 logo">
-                <img :src="'/images/logotype.png'" alt="logo Phlox">
-            </div>
-            <div class="col-lg-8 col-md-7 col-sm-7">
-                <ul class="nav d-flex justify-content-end">
-                    <li class="nav-item d-flex" v-for="(item, navIndex) in this.store.navBar">
-                        <a class="nav-link pt-4 mx-2" aria-current="page" href="#"
-                            :class="{ 'border-active': item.active === true }">{{
-                                item.navItem }}</a>
-                    </li>
-                </ul>
+    <header>
+        <div class="container-lg">
+            <div class="row align-items-center">
+                <div class="col-lg-2 col-md-4 col-sm-3 logo">
+                    <img :src="'/images/logotype.png'" alt="logo Phlox">
+                </div>
+                <div class="col-lg-10 col-md-7 col-sm-9">
+                    <ul class="nav d-flex justify-content-end">
+                        <li class="nav-item d-flex" v-for="(item, navIndex) in this.store.navBar">
+                            <a class="nav-link pt-4 mx-2 text-light" aria-current="page" href="#"
+                                :class="{ 'border-active': item.active === true }">{{
+                                    item.navItem }}</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-
-    </div>
+    </header>
 </template>
 
 <script>
@@ -33,7 +34,18 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/style/partials/variables' as *;
 
+header {
+    background-image: url('../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg');
+    background-position: right;
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 130vh;
+    background-color: $blueHeader;
+}
+
 .logo {
+    background-color: $blueHeader;
+
     img {
         width: auto;
         height: 20px;
@@ -41,6 +53,6 @@ export default {
 }
 
 .border-active {
-    border-top: 3px solid red
+    border-top: 3px solid $lightColor;
 }
 </style>

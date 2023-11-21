7991ef7c2ab4397482fad859205c90a4b2a9786c
ericaostini <ericaostini14@gmail.com>
2023-11-21 15:29:44 +0100

    create object array for card in consultant section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Header />
+  <Main />
 </template>
 
 <script>
 import { store } from './components/data/store';
 import Header from './components/Header.vue';
+import Main from './components/Main.vue';
 export default {
   name: "App",
   data() {
@@ -12,7 +14,7 @@ export default {
       store
     };
   },
-  components: { Header }
+  components: { Header, Main }
 }
 </script>
 
---
+++ src/components/Main.vue
@@ -0,0 +1,42 @@
+<template>
+    <main>
+        <section id="consultant">
+            <div class="container">
+                <h4>Our consultants can help you</h4>
+                <div class="border-green"></div>
+            </div>
+            <div class="card border" v-for="(consultant, cIndex) in this.store.consultants">
+                <h5>{{ consultant.title }}</h5>
+                <p>{{ consultant.text }}</p>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+import { store } from './data/store';
+export default {
+    name: 'Main',
+    data() {
+        return {
+            store
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/style/partials/variables' as *;
+
+#consultant {
+    width: 95%;
+    float: right;
+    background-color: $lightColor;
+
+    .border-green {
+        width: 4%;
+        height: 4px;
+        border: 2px solid $mainGreen;
+    }
+}
+</style>

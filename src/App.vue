5862bda9eed5d3e9780b9a67b530d5bdfb503373
ericaostini <ericaostini14@gmail.com>
2023-11-21 11:40:25 +0100

    start project commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    ciao
+  </div>
+</template>
+
+<script>
+import { store } from './components/data/store';
+export default {
+  name: 'App',
+  data() {
+    return {
+      store
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>

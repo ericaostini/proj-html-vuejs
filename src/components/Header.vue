f40991a0d6c65b21dfd14a6e70dcc8f31fe15041
ericaostini <ericaostini14@gmail.com>
2023-11-21 12:27:45 +0100

    nav bar responsive layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-4 logo">
+            <div class="col-lg-4 col-md-5 col-sm-5 logo">
                 <img :src="'/images/logotype.png'" alt="logo Phlox">
             </div>
-            <div class="col-8">
+            <div class="col-lg-8 col-md-7 col-sm-7">
                 <ul class="nav d-flex justify-content-end">
                     <li class="nav-item d-flex" v-for="(item, navIndex) in this.store.navBar">
                         <a class="nav-link pt-4 mx-2" aria-current="page" href="#"

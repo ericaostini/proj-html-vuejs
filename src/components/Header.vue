48eef626a0d6d5320430d30b7d3de29d08e05422
ericaostini <ericaostini14@gmail.com>
2023-11-21 15:08:20 +0100

    style title and bottom in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
-        <div class="container-lg">
-            <div class="row align-items-center">
+        <div class="px-4">
+            <div class="row align-items-center justify-content-between">
                 <div class="col-lg-2 col-md-4 col-sm-3 logo">
                     <img :src="'/images/logotype.png'" alt="logo Phlox">
                 </div>
@@ -16,11 +16,16 @@
                 </div>
             </div>
         </div>
-        <div class="text-light text-uppercase w-50">
-            <h5>Always the best way you need it</h5>
-            <h1>The best business consulting</h1>
-            <button class="btn btn-success">read more</button>
-            <i class="fa-regular fa-circle-play"></i>
+        <div class="text-light text-uppercase title-header container">
+            <div class="d-flex align-items-center px-3">
+                <div class="border-green mb-2"></div>
+                <h5 class="px-3">Always the best way you need it</h5>
+            </div>
+            <h1 class="px-3">The best business consulting</h1>
+            <div class="px-3 d-flex align-items-center pt-5">
+                <button class="btn text-uppercase">read more</button>
+                <i class="fa-regular fa-circle-play p-3"></i>
+            </div>
         </div>
     </header>
 </template>
@@ -41,22 +46,54 @@ export default {
 @use '../assets/style/partials/variables' as *;
 
 header {
+    position: relative;
     background-image: url('../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg');
     background-position: right;
-    background-size: contain;
+    background-size: 1100px 1000px;
     background-repeat: no-repeat;
-    height: 130vh;
-    background-color: $blueHeader;
-}
-
-.logo {
+    height: 1000px;
+    width: 100%;
     background-color: $blueHeader;
 
-    img {
-        width: auto;
-        height: 20px;
+    .logo {
+        background-color: $blueHeader;
+
+        img {
+            width: auto;
+            height: 20px;
+        }
+    }
+
+    .title-header {
+        position: absolute;
+        top: 300px;
+        width: 45%;
+
+        .border-green {
+            width: 4%;
+            height: 4px;
+            border: 2px solid $mainGreen;
+        }
+
+        h1 {
+            font-size: 100px;
+            line-height: 90px;
+        }
+
+        button {
+            background-color: $mainGreen;
+            width: 40%;
+            padding: 28px;
+            border-radius: 1px;
+        }
+
+        i {
+            font-size: 50px;
+        }
+
     }
 }
+
 
 .border-active {
     border-top: 3px solid $lightColor;

fb641e1b49d96dc39e9d3c317a777185f383e5b7
ericaostini <ericaostini14@gmail.com>
2023-11-23 11:32:42 +0100

    add botton chat

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <Header />
   <Main />
   <Footer />
+  <button class="btn btn-light"><i class="fa-regular fa-comment"></i></button>
 </template>
 
 <script>
@@ -20,4 +21,26 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use './assets/style/partials/variables' as *;
+
+button {
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  z-index: 100;
+  padding: 12px 15px;
+  background-color: $chatColor;
+  border-radius: 50px;
+  border-color: $chatColor;
+
+  i {
+    font-size: 30px;
+    color: $lightColor;
+  }
+
+  &:hover i {
+    color: black;
+  }
+}
+</style>
